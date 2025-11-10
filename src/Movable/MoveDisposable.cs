ec11b783052cad31e9b378b5cba789bac973dc69
Changjiang Yang <changjiangyang@noahmed.com>
2025-11-10 12:57:28 -0500

    add exception message

--- src/Movable/MoveDisposable.cs
+++ src/Movable/MoveDisposable.cs
@@ -1,15 +1,16 @@
 ﻿namespace Movable
 {
 
-    public class MoveDisposable<TResource>(TResource resource) : IDisposable where TResource : class, IDisposable
+    public sealed class MoveDisposable<TResource>(TResource resource) : IDisposable where TResource : class, IDisposable
     {
+        private const string invalidMoveMessage = "Resource has already been moved.";
         private TResource? resource = resource ?? throw new ArgumentNullException(nameof(resource));
 
-        public TResource Value => resource ?? throw new InvalidOperationException();
+        public TResource Value => resource ?? throw new InvalidOperationException(invalidMoveMessage);
 
         public TResource Move()
         {
-            TResource result = resource ?? throw new InvalidOperationException();
+            TResource result = resource ?? throw new InvalidOperationException(invalidMoveMessage);
             resource = null;
             return result;
         }
